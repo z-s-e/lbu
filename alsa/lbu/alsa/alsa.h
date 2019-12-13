ee7cd2b45d4e30e966741c7685a78166cbdec2a0
Zeno Endemann <zeno.endemann@googlemail.com>
2019-12-13 21:35:55 +0100

    SPDX

--- alsa/lbu/alsa/alsa.h
+++ alsa/lbu/alsa/alsa.h
@@ -1,20 +1,5 @@
 /* Copyright 2015-2016 Zeno Sebastian Endemann <zeno.endemann@googlemail.com>
- *
- * This file is part of the lbu library.
- *
- * lbu is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the licence, or (at your option) any later version.
- *
- * lbu is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #ifndef LIBLBU_ALSA_H
@@ -105,7 +90,7 @@ namespace alsa {
 
         static int device_flags(const char* name);
 
-        struct hw_device { char name[8]; };
+        struct hw_device { char name[8]; }; // TODO change to array?
         static hw_device device_for_card_index(int index);
 
     private:
